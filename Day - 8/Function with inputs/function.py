39f0c1ee90ed87e2d61d10d826e8da0517e675c1
Ram Charan <rcyerramsetti@gmail.com>
2025-08-22 22:10:48 +0530

    updated

---
+++ Day - 8/Function with inputs/function.py
@@ -0,0 +1,6 @@
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}")
+    print(f"Where do you stay {name}")
+
+greet_with_name("ram")
---
+++ Day - 8/Position vs Keyword Argument/main.py
@@ -0,0 +1,6 @@
+def greet_with_name(name,location):
+    print(f"Hello {name}")
+    print(f"what is it staying in {location}")
+
+greet_with_name("Ram","India")
+greet_with_name(location = "India",name = "Ram") 
