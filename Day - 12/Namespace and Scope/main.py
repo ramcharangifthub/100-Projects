fc1e2cb77d9f3b57eb440460db1a40c1f1505ea5
Ram Charan <rcyerramsetti@gmail.com>
2025-08-28 09:16:14 +0530

    updated

---
+++ Day - 12/Namespace and Scope/main.py
@@ -0,0 +1,10 @@
+enemies = 1
+
+
+def increase_enimies():
+    enemies = 2
+    print(f"Enemies inside the function is {enemies}")
+
+increase_enimies()
+
+print(f"Enemies outside the function is {enemies}")
