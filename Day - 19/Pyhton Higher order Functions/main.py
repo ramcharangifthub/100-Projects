fdc88be309c2dfd35c926900d3b8bb4b012c3887
Ram Charan <rcyerramsetti@gmail.com>
2025-09-10 22:35:50 +0530

    updated

---
+++ Day - 19/Pyhton Higher order Functions/main.py
@@ -0,0 +1,28 @@
+from turtle import Turtle,Screen
+tim = Turtle()
+screen = Screen()
+
+def mov_forwards():
+    tim.forward(10)
+def mov_backwards():
+    tim.backward(10)
+def turn_left():
+    new_heading = tim.heading()+10
+    tim.setheading(new_heading)
+def turn_right():
+    new_heading = tim.heading()-10
+    tim.setheading(new_heading)
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(mov_forwards,"w")
+screen.onkey(mov_backwards,"s")
+screen.onkey(turn_left,"a")
+screen.onkey(turn_right,"d")
+screen.onkey(clear,"c")
+screen.exitonclick()
+
